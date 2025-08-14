65107c12963fc14e3c8208a75e811affeef86a97
Aniket Agrawal <ankt7987@gmail.com>
2025-08-14 16:10:21 +0530

    Project Initialise

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const indexRoutes = require('./routes/index.routes')
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public')); //this serves static files from the public directory
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', indexRoutes);
+
+module.exports = app;
